28e6685b134a6d7e79cf094dba1299c3989933f3
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-07-29 15:30:47 +0600

    project setup is done

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+
+const createApp = (auth) => {
+    const app = express();
+
+    app.use(cors({
+        origin: [
+            process.env.FRONTEND_URL,
+            "http://localhost:3000",
+        ].filter(Boolean),
+        credentials: true
+    }));
+
+    app.use(cookieParser());
+    app.use(express.json());
+
+    app.all("/api/auth/*splat", toNodeHandler(auth));
+
+    app.get("/", (req, res) => {
+        res.send("M traders server is running successfully");
+    });
+
+    return app;
+}
+
+export default createApp;
